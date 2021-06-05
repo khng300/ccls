9999044e986055a8121f12a86798b6d9d0900d76
Ka Ho Ng <khng300@gmail.com>
2021-06-05 19:55:03 +0800

    Switched pointer

--- src/db_allocator.hh
+++ src/db_allocator.hh
@@ -15,7 +15,259 @@ namespace ccls {
 namespace db {
 namespace impl {
 
-template <typename T> using offset_ptr = boost::interprocess::offset_ptr<T>;
+template <typename T> class offsetPointer {
+  ptrdiff_t off;
+
+  struct nat {};
+
+public:
+  typedef T element_type;
+  typedef typename std::remove_cv<T>::type value_type;
+  typedef T *pointer;
+  typedef std::add_lvalue_reference_t<
+      std::conditional_t<std::is_void_v<element_type>, nat, element_type>>
+      reference;
+  typedef std::random_access_iterator_tag iterator_category;
+  typedef ptrdiff_t difference_type;
+
+  offsetPointer() noexcept = default;
+  offsetPointer(const offsetPointer &p) noexcept { copyAssign(p); }
+  template <typename T2>
+  offsetPointer(const offsetPointer<T2> &p,
+                std::enable_if_t<std::is_convertible_v<T2 *, pointer>,
+                                 pointer> = nullptr) noexcept {
+    assignRawPtr(p.rawPtr());
+  }
+  template <typename T2>
+  explicit offsetPointer(const offsetPointer<T2> &p,
+                         std::enable_if_t<!std::is_convertible_v<T2 *, pointer>,
+                                          pointer> = nullptr) noexcept {
+    assignRawPtr(p.rawPtr());
+  }
+  offsetPointer(pointer p) noexcept { assignRawPtr(p); }
+  template <typename T2>
+  offsetPointer(T2 *p, std::enable_if_t<std::is_convertible_v<T2 *, pointer>,
+                                        pointer> = nullptr) noexcept {
+    assignRawPtr(p);
+  }
+  template <typename T2>
+  explicit offsetPointer(
+      T2 *p, std::enable_if_t<!std::is_convertible_v<T2 *, pointer>, pointer> =
+                 nullptr) noexcept {
+    assignRawPtr(p);
+  }
+
+  offsetPointer &operator=(const offsetPointer &rhs) noexcept {
+    copyAssign(rhs);
+    return *this;
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  offsetPointer &operator=(const offsetPointer<T2> &rhs) noexcept {
+    assignRawPtr(rhs.rawPtr());
+    return *this;
+  }
+  offsetPointer &operator=(pointer rhs) noexcept {
+    assignRawPtr(rhs);
+    return *this;
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  offsetPointer &operator=(pointer rhs) noexcept {
+    assignRawPtr(rhs);
+  }
+
+  reference operator*() const noexcept { return *rawPtr(); }
+
+  pointer operator->() const noexcept { return rawPtr(); }
+
+  reference operator[](difference_type idx) const noexcept {
+    return rawPtr()[idx];
+  }
+
+  offsetPointer &operator+=(difference_type diff) {
+    off += step(diff);
+    return *this;
+  }
+  offsetPointer &operator-=(difference_type diff) {
+    off -= step(diff);
+    return *this;
+  }
+
+  offsetPointer &operator++() noexcept {
+    operator+=(1);
+    return *this;
+  }
+  offsetPointer operator++(int) const noexcept {
+    offsetPointer t(*this);
+    t += 1;
+    return *this;
+  }
+
+  offsetPointer &operator--() noexcept {
+    operator-=(1);
+    return *this;
+  }
+  offsetPointer operator--(int) const noexcept {
+    offsetPointer t(*this);
+    t -= 1;
+    return *this;
+  }
+
+  friend offsetPointer operator+(difference_type diff,
+                                 offsetPointer rhs) noexcept {
+    rhs += diff;
+    return rhs;
+  }
+  offsetPointer operator+(difference_type diff) const noexcept {
+    offsetPointer lhs(*this);
+    lhs += diff;
+    return lhs;
+  }
+
+  friend offsetPointer operator-(difference_type diff,
+                                 offsetPointer rhs) noexcept {
+    rhs -= diff;
+    return rhs;
+  }
+  offsetPointer operator-(difference_type diff) const noexcept {
+    offsetPointer lhs(*this);
+    lhs -= diff;
+    return lhs;
+  }
+  friend difference_type operator-(const offsetPointer &lhs,
+                                   const offsetPointer &rhs) {
+    return lhs.rawPtr() - rhs.rawPtr();
+  }
+
+  bool operator>(const offsetPointer &rhs) const noexcept {
+    return rawPtr() > rhs.rawPtr();
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator>(const offsetPointer<T2> &rhs) const noexcept {
+    return rawPtr() > rhs.rawPtr();
+  }
+  bool operator>(pointer rhs) const noexcept { return rawPtr() > rhs; }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator>(T2 *rhs) const noexcept {
+    return rawPtr() > rhs;
+  }
+
+  bool operator>=(const offsetPointer &rhs) const noexcept {
+    return rawPtr() >= rhs.rawPtr();
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator>=(const offsetPointer<T2> &rhs) const noexcept {
+    return rawPtr() >= rhs.rawPtr();
+  }
+  bool operator>=(pointer rhs) const noexcept { return rawPtr() >= rhs; }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator>=(T2 *rhs) const noexcept {
+    return rawPtr() >= rhs;
+  }
+
+  bool operator<(const offsetPointer &rhs) const noexcept {
+    return rawPtr() < rhs.rawPtr();
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator<(const offsetPointer<T2> &rhs) const noexcept {
+    return rawPtr() < rhs.rawPtr();
+  }
+  bool operator<(pointer rhs) const noexcept { return rawPtr() < rhs; }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator<(T2 *rhs) const noexcept {
+    return rawPtr() < rhs;
+  }
+
+  bool operator<=(const offsetPointer &rhs) const noexcept {
+    return rawPtr() <= rhs.rawPtr();
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator<=(const offsetPointer<T2> &rhs) const noexcept {
+    return rawPtr() <= rhs.rawPtr();
+  }
+  bool operator<=(pointer rhs) const noexcept { return rawPtr() <= rhs; }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator<=(T2 *rhs) const noexcept {
+    return rawPtr() <= rhs;
+  }
+
+  bool operator==(const offsetPointer &rhs) const noexcept {
+    return rawPtr() == rhs.rawPtr();
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator==(const offsetPointer<T2> &rhs) const noexcept {
+    return rawPtr() == rhs.rawPtr();
+  }
+  bool operator==(pointer rhs) const noexcept { return rawPtr() == rhs; }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator==(T2 *rhs) const noexcept {
+    return rawPtr() == rhs;
+  }
+
+  bool operator!=(const offsetPointer &rhs) const noexcept {
+    return !operator==(rhs);
+  }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator!=(const offsetPointer<T2> &rhs) const noexcept {
+    return !operator==(rhs);
+  }
+  bool operator!=(pointer rhs) const noexcept { return !operator==(rhs); }
+  template <typename T2,
+            typename = std::enable_if_t<std::is_convertible_v<T2 *, pointer>>>
+  bool operator!=(T2 *rhs) const noexcept {
+    return !operator==(rhs);
+  }
+
+  explicit operator bool() const noexcept { return off != 0; }
+
+  pointer rawPtr() const noexcept {
+    uintptr_t o = off;
+    uintptr_t is_null = o == 0, is_self = o == 1;
+    --is_null;
+    --is_self;
+    o &= is_self;
+    uintptr_t raw = uintptr_t(this) + o;
+    raw &= is_null;
+    return reinterpret_cast<pointer>(raw);
+  }
+
+  pointer get() const noexcept { return rawPtr(); }
+
+  static constexpr offsetPointer pointer_to(reference r) noexcept {
+    return std::addressof(r);
+  }
+
+private:
+  void copyAssign(const offsetPointer &rhs) noexcept {
+    assignRawPtr(rhs.rawPtr());
+  }
+
+  void assignRawPtr(const void *raw) noexcept {
+    uintptr_t diff = uintptr_t(raw) - uintptr_t(this);
+    uintptr_t self_off = diff == 0, is_null = raw == nullptr;
+    --is_null;
+    off = (diff + self_off) & is_null;
+  }
+
+  static constexpr ptrdiff_t step(ptrdiff_t i) {
+    return i * sizeof(value_type);
+  }
+};
+
+// template <typename T> using offset_ptr = boost::interprocess::offset_ptr<T>;
+template <typename T> using offset_ptr = offsetPointer<T>;
 
 struct MutexFamily {
   typedef boost::interprocess::ipcdetail::spin_mutex mutex_type;
